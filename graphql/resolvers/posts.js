23cd2212c36bf66fc021f72bf30b3dd668791701
Chinmay anand <chinmayanand896@gmail.com>
2020-08-31 22:03:30 +0530

    deleteComment add

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -94,6 +94,26 @@ module.exports = {
             }else {
                 throw new UserInputError('Post not found.');
             }
+        },
+
+        async deleteComment (_, { postId, commentId }, context) {
+            const { username } = checkAuth(context);
+
+            const post = await Post.findById(postId);
+
+            if (post) {
+                const commentIndex = post.comments.findIndex( c => c.id === commentId);
+
+                if (post.comments[commentIndex].username === username) {
+                    post.comments.splice(commentIndex, 1);
+                    await post.save();
+                    return post;
+                }else {
+                    throw new AuthenticationError('Action is not allowed');
+                }
+            } else {
+                throw new UserInputError('Post not found.');
+            }
         }
     }
 }
